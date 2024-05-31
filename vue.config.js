d14c7512f213cfbd41d5e67cbfc3b212d48cedac
hkp <colourful4570@163.com>
2024-05-31 11:03:37 +0800

    vue和webpack版本不兼容问题修复

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
 const {defineConfig} = require('@vue/cli-service')
 
 module.exports = defineConfig({
-    transpileDependencies: true,
-    devServer: {
+    transpileDependencies: true, devServer: {
         proxy: {
             "/v1": {
                 target: "http://cangdu.org:8001"
+            }
+        }
+    }, configureWebpack: {
+        resolve: {
+            fallback: {
+                'assert': false,
+                'process': false,
+                'url': false,
+                'path-browserify': false,
+                'util': false,
+                "path": false,
+                "fs": false,
+                "module": false,
+                "v8": false,
             }
         }
     }
